3c8f26c6e6f489e558eabaaf9e5a31034c2bf0cb
syu danjo <syu56981@gmail.com>
2022-07-11 19:48:12 +0900

    feat: next.js学習用

---
+++ next-test/pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from './components/layout.js';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+        {/*<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>*/}
+        {/*<script src="./chart/mychart.js"></script>*/}
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[檀上珠宇です。息子が1歳になりました。]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
---
+++ next-test/pages/posts/[id].js
@@ -0,0 +1,31 @@
+import Layout from '../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false, //指定パス以外なら404を返す trueなら用意したページを見せることが出来る
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
